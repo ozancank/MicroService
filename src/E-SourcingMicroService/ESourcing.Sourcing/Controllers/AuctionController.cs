d6fcf72a096e15f158f0dd1038d03498fd2ba85f
OZAN CAN KOSEMEZ <ozancank@gmail.com>
2021-12-23 12:50:28 +0300

    .

--- src/E-SourcingMicroService/ESourcing.Sourcing/Controllers/AuctionController.cs
+++ src/E-SourcingMicroService/ESourcing.Sourcing/Controllers/AuctionController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ESourcing.Sourcing.Entities;
+using ESourcing.Sourcing.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace ESourcing.Sourcing.Controllers
 {
@@ -6,6 +12,43 @@ namespace ESourcing.Sourcing.Controllers
     [ApiController]
     public class AuctionController : ControllerBase
     {
-       
+        private readonly IAuctionRepository _auctionRepository;
+        private readonly ILogger<AuctionController> _logger;
+
+        public AuctionController(IAuctionRepository auctionRepository, ILogger<AuctionController> logger)
+        {
+            _auctionRepository = auctionRepository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Auction>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Auction>>> GetAuctions()
+        {
+            var auctions = await _auctionRepository.GetAuctions();
+            return Ok(auctions);
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetAuction")]
+        [ProducesResponseType(typeof(Auction), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult<Auction>> GetAuction(string id)
+        {
+            var auction = await _auctionRepository.GetAuction(id);
+            if (auction == null)
+            {
+                _logger.LogError($"Auction with id: {id}, hasn't been found in database");
+                return NotFound();
+            }
+            return Ok(auction);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Auction), (int)HttpStatusCode.Created)]
+        public async Task<ActionResult<Auction>> CreateAuctions([FromBody] Auction auction)
+        {
+            await _auctionRepository.Create(auction);
+            return CreatedAtRoute("GetAuction", new { id = auction.Id }, auction);
+        }
     }
 }
---
+++ src/E-SourcingMicroService/ESourcing.Sourcing/Repositories/Interfaces/IBidRepository.cs
@@ -0,0 +1,15 @@
+﻿using ESourcing.Sourcing.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ESourcing.Sourcing.Repositories.Interfaces
+{
+    public interface IBidRepository
+    {
+        Task SendBid(Bid bid);
+
+        Task<List<Bid>> GetBidsAuctionId(string id);
+
+        Task<Bid> GetWinnerBid(string id);
+    }
+}
