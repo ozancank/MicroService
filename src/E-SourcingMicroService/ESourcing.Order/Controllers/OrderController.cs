00d15a6a4b1efd3ae8dafbcf6e94245726009f9c
OZAN CAN KOSEMEZ <ozancank@gmail.com>
2021-12-24 16:55:20 +0300

    .

---
+++ src/E-SourcingMicroService/ESourcing.Order/Controllers/OrderController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESourcing.Order.Controllers
+{
+    [Route("api/v1/[]controller")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+    }
+}
---
+++ src/E-SourcingMicroService/Ordering.Domain/Repositories/IOrderRepository.cs
@@ -0,0 +1,12 @@
+﻿using Ordering.Domain.Entities;
+using Ordering.Domain.Repositories.Base;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Repositories
+{
+    public interface IOrderRepository : IRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetOrdersBySellerUserName(string userName);
+    }
+}
