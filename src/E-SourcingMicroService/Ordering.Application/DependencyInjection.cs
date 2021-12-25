e65b16791637dba754832e7ca6e109d57f1e0562
OZAN CAN KOSEMEZ <ozancank@gmail.com>
2021-12-25 08:49:43 +0300

    .

--- src/E-SourcingMicroService/Ordering.Application/DependencyInjection.cs
+++ src/E-SourcingMicroService/Ordering.Application/DependencyInjection.cs
@@ -1,6 +1,8 @@
-﻿using FluentValidation;
+﻿using AutoMapper;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Mapper;
 using System.Reflection;
 
 namespace Ordering.Application
@@ -12,6 +14,17 @@ namespace Ordering.Application
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
             services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            #region Configure Mapper
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
+                cfg.AddProfile<OrderMappingProfile>();
+            });
+            var mapper = config.CreateMapper();
+
+            #endregion Configure Mapper
 
             return services;
         }
