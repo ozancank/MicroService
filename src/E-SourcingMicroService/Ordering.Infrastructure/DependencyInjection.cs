1a3159e735fb3cac24c44a25bc865ac89ceef20d
ozancank <ozancank@gmail.com>
2021-12-26 17:28:59 +0300

    .

--- src/E-SourcingMicroService/Ordering.Infrastructure/DependencyInjection.cs
+++ src/E-SourcingMicroService/Ordering.Infrastructure/DependencyInjection.cs
@@ -13,9 +13,13 @@ namespace Ordering.Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContext<OrderContext>(opt => opt.UseInMemoryDatabase(databaseName: "InMemoryDb"),
-                                                ServiceLifetime.Singleton,
-                                                ServiceLifetime.Singleton);
+            //services.AddDbContext<OrderContext>(opt => opt.UseInMemoryDatabase(databaseName: "InMemoryDb"),
+            //                                    ServiceLifetime.Singleton,
+            //                                    ServiceLifetime.Singleton);
+
+            services.AddDbContext<OrderContext>(opt => opt.UseSqlServer(
+                configuration.GetConnectionString("OrderConnection"),
+                b => b.MigrationsAssembly(typeof(OrderContext).Assembly.FullName)), ServiceLifetime.Singleton);
 
             services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
             services.AddTransient<IOrderRepository, OrderRepository>();
