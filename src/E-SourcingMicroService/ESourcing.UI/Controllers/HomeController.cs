f918702bf721c11366ea62e247f784b2d80ef1fc
OZAN CAN KOSEMEZ <ozancank@gmail.com>
2021-12-27 10:32:56 +0300

    .

--- src/E-SourcingMicroService/ESourcing.UI/Controllers/HomeController.cs
+++ src/E-SourcingMicroService/ESourcing.UI/Controllers/HomeController.cs
@@ -1,10 +1,24 @@
-﻿using ESourcing.UI.ViewModel;
+﻿using ESourcing.Core.Entities;
+using ESourcing.UI.ViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace ESourcing.UI.Controllers
 {
     public class HomeController : Controller
     {
+        public UserManager<AppUser> _userManager { get; }
+        public SignInManager<AppUser> _signInManager { get; }
+
+        public HomeController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        //[Authorize]
         public IActionResult Index()
         {
             return View();
@@ -16,8 +30,25 @@ namespace ESourcing.UI.Controllers
         }
 
         [HttpPost]
-        public IActionResult Login(LoginViewModel loginModel)
+        public async Task<IActionResult> Login(LoginViewModel loginModel, string returnUrl)
         {
+            returnUrl ??= Url.Content("~/");
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(loginModel.Email);
+                if (user != null)
+                {
+                    await _signInManager.SignOutAsync();
+                    var result = await _signInManager.PasswordSignInAsync(user, loginModel.Password, false, false);
+                    if (result.Succeeded)
+                        //return RedirectToAction("Index");
+                        return LocalRedirect(returnUrl);
+                    else
+                        ModelState.AddModelError("", "Email address is not valid Or password");
+                }
+                else
+                    ModelState.AddModelError("", "Email address is not valid Or password");
+            }
             return View();
         }
 
@@ -27,9 +58,45 @@ namespace ESourcing.UI.Controllers
         }
 
         [HttpPost]
-        public IActionResult Signup(AppUserViewModel signupModel)
+        public async Task<IActionResult> Signup(AppUserViewModel signupModel)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                AppUser usr = new();
+                usr.FirstName = signupModel.FirstName;
+                usr.Email = signupModel.Email;
+                usr.LastName = signupModel.LastName;
+                usr.PhoneNumber = signupModel.PhoneNumber;
+                usr.UserName = signupModel.UserName;
+                if (signupModel.UserSelectTypeId == 1)
+                {
+                    usr.IsBuyer = true;
+                    usr.IsSeller = false;
+                }
+                else
+                {
+                    usr.IsBuyer = false;
+                    usr.IsSeller = true;
+                }
+
+                var result = await _userManager.CreateAsync(usr, signupModel.Password);
+
+                if (result.Succeeded) return RedirectToAction("Login");
+                else
+                {
+                    foreach (IdentityError item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(signupModel);
+        }
+
+        public IActionResult Logout()
+        {
+            _signInManager.SignOutAsync(); ;
+            return RedirectToAction("Login");
         }
     }
 }
--- src/E-SourcingMicroService/ESourcing.UI/Startup.cs
+++ src/E-SourcingMicroService/ESourcing.UI/Startup.cs
@@ -1,5 +1,9 @@
 using ESourcing.Core.Entities;
+using ESourcing.Core.Repositories;
+using ESourcing.Core.Repositories.Base;
 using ESourcing.Infrastructure.Data;
+using ESourcing.Infrastructure.Repository;
+using ESourcing.Infrastructure.Repository.Base;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -7,6 +11,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace ESourcing.UI
 {
@@ -22,11 +27,41 @@ namespace ESourcing.UI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<WebAppContext>(options => options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection")));
-            services.AddIdentity<AppUser, IdentityRole>().AddDefaultTokenProviders().AddEntityFrameworkStores<WebAppContext>();
+            services.AddIdentity<AppUser, IdentityRole>(opt =>
+            {
+                opt.Password.RequiredLength = 4;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireDigit = false;
+            }).AddDefaultTokenProviders().AddEntityFrameworkStores<WebAppContext>();
 
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddMvc();
             services.AddRazorPages();
+            services.AddSession(opt =>
+            {
+                opt.IdleTimeout = TimeSpan.FromMinutes(20);
+            });
+
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped(typeof(IUserRepository), typeof(UserRepository));
+
+            //services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            //        .AddCookie(options =>
+            //        {
+            //            options.Cookie.Name = "My Cookie";
+            //            options.LoginPath = "Home/Login";
+            //            options.LogoutPath = "Home/Logout";
+            //            options.ExpireTimeSpan = TimeSpan.FromDays(3);
+            //            options.SlidingExpiration = false;
+            //        });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = $"/Home/Login";
+                options.LogoutPath = $"/Home/Logout";
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -46,14 +81,13 @@ namespace ESourcing.UI
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-
+                endpoints.MapControllerRoute(name: "default",
+                      pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
